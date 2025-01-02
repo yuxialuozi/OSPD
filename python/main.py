af9f8c672736efc01cac8816012ee7ec56973dc1
yuxialuozi <3159452953@qq.com>
2025-01-02 18:38:19 +0800

    feat：python basic code

---
+++ python/main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(name)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('今天是我的第一个提交')
+
+
+
